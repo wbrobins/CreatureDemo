eb3335ee34f39ff336348dc82759000e267cff89
blazetbd <blazetbd@gmail.com>
2025-11-03 16:47:51 -0500

    Add proper player sprites and animations

--- Assets/Scripts/World/PlayerController.cs
+++ Assets/Scripts/World/PlayerController.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private Vector2 input;
+    [SerializeField] private Vector2 input;
+
+    private Vector2 lastDirection = Vector2.down;
+    
+    [SerializeField] private Animator playerAnimator;
 
     public void HandleUpdate()
     {
@@ -12,8 +16,20 @@ public class PlayerController : MonoBehaviour
 
         if (input != Vector2.zero)
         {
+            lastDirection = input;
             transform.Translate(input * moveSpeed * Time.deltaTime);
+            playerAnimator.SetBool("isMoving", true);
+            playerAnimator.SetFloat("moveX", input.x);
+            playerAnimator.SetFloat("moveY", input.y);
         }
+        else
+        {
+            playerAnimator.SetBool("isMoving", false);
+            playerAnimator.SetFloat("moveX", lastDirection.x);
+            playerAnimator.SetFloat("moveY", lastDirection.y);
+        }
+
+        Debug.Log(playerAnimator.GetBool("isMoving") + "X: " + playerAnimator.GetFloat("moveX") + " Y: " + playerAnimator.GetFloat("moveY"));
     }
 
     void OnTriggerEnter2D(Collider2D collision)
